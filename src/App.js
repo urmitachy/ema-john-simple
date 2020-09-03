4bcc1d47d2905fa5567e57b813e03a802c14af72
urmitachy <ucmoon0703@gmail.com>
2020-09-03 21:05:21 +0600

    ema-john-simple done

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+
+
+function App() {
+  return (
+    <div>
+    <Header />
+      
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Shop />
+        </Route>
+        <Route path="/shop">
+          <Shop />
+        </Route>
+        <Route path="/review">
+          <Review />
+        </Route>
+        <Route path="/inventory">
+          <Inventory />
+        </Route>
+        <Route path="/product/:productKey">
+          <ProductDetail />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+import { Link } from 'react-router-dom';
+
+
+const Product = (props) => {
+    // console.log(props);
+    const{img, name, seller,price,stock, key}= props.product;
+    return (
+        
+        <div className = 'product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name"><Link to ={"/product/"+key}>{name}</Link></h4>
+                 <br/>
+                 <p><small>by : {seller}</small></p>
+                 <p>${price}</p>
+                 <br/>
+                <p><small>only{stock}left in stock = order soon </small></p>
+               {props.showAddToCart === true && <button className = "main-button"
+                    onClick = {() => props.handleAddProduct(props.product)}>
+                    <FontAwesomeIcon icon={faShoppingCart} />
+                 Add to cart</button>}
+            </div>
+           
+        </div>
+    );
+};
+
+export default Product;
